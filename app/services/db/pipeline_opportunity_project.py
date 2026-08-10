cb69430d635dd8b75dbf79055f89502ff5ae3337
ahameed-benten <nanisriramnanda@gmail.com>
2026-08-10 18:29:31 +0530

    Removed relavent projects field in sales enablement

--- app/services/db/pipeline_opportunity_project.py
+++ app/services/db/pipeline_opportunity_project.py
@@ -39,6 +39,18 @@ async def get_pipeline_opportunity_project_by_id(db: AsyncSession, pipeline_oppo
         raise e
 
 
+async def get_pipeline_opportunity_project_by_opportunity_id(db: AsyncSession, pipeline_opportunity_id: UUID):
+    try:
+        result = await db.execute(
+            select(PipelineOpportunityProjectModel).where(PipelineOpportunityProjectModel.opportunity_id == pipeline_opportunity_id)
+        )
+        return result.scalars().all()
+    except SQLAlchemyError as e:
+        await db.rollback()
+        logging.exception("Could not find Pipeline Opportunity Project by opportunity id")
+        raise e
+
+
 async def create_pipeline_opportunity_project(db: AsyncSession, pipeline_opportunity_project: PipelineOpportunityProjectModel):
     try:
         db.add(pipeline_opportunity_project)
